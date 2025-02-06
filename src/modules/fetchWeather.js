17eef75cf792e9f9ea55b7f39372bda8f62e3f69
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-02-06 11:38:09 -0500

    Add function to get necessary information from API (#1)

---
+++ src/modules/fetchWeather.js
@@ -0,0 +1,43 @@
+async function getWeatherData (query, unit) {
+
+    if(query === '' || query === null || query === undefined) query = 'london';
+    if(unit === '' || unit === null || unit === undefined) unit = 'metric';
+    const key = 'YHGBZSMDEJV8TMA7YHEFQWVZF';
+
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(query)}?unitGroup=${unit}&key=${key}`, {
+            mode: 'cors',
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (!response.ok) {
+            throw new Error('Error en la respuesta de la API');
+        }
+
+        const weatherData = await response.json();
+        
+        const weather = {
+            address: weatherData.resolvedAddress, 
+            description: weatherData.description, 
+            datetime: weatherData.currentConditions.datetime,
+            conditions: weatherData.currentConditions.conditions,
+            feelslike: weatherData.currentConditions.feelslike,
+            humidity: weatherData.currentConditions.humidity,
+            sunrise: weatherData.currentConditions.sunrise,
+            sunset: weatherData.currentConditions.sunset,
+            uv: weatherData.currentConditions.uvindex,
+            temp: weatherData.currentConditions.temp,
+            windspeed: weatherData.currentConditions.windspeed,
+            week: weatherData.days.slice(0, 6),
+        };
+
+        return weather;
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export default getWeatherData;
