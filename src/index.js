1ced0880e8ced9e94e8ab4fcc29106085238a4b3
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-02-18 18:27:12 -0500

    Layout of the html structure and search bar

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+import fetchWeather from './modules/fetchWeather.js';
+import Magnify from './resources/icons/magnify.svg';
+
+document.getElementById('search-icon').src = Magnify;
